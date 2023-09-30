56ceaf05971cc0eaade17169cd0571d4f85bd0e0
pervin agayev <pervinagayev28@gmail.com>
2023-09-30 12:17:25 +0400

    Practic

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Text.Json;
+
+
+public class Course
+{
+    public string course { get; set; }
+    public override string ToString()
+    {
+        return $"\n\tCourse: {course}";
+    }
+}
+
+public class Person
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string surname { get; set; }
+    public string gen { get; set; }
+    public List<Course> courses { get; set; }
+    public override string ToString()
+    {
+        string coursesStr = string.Join(", ", courses.Select(c => c.ToString()));
+        return $"ID: {id}\n, Name: {name}\n, Surname: {surname}\n, Gender: {gen}\n, Courses: {coursesStr}\n";
+    }
+}
+
+public class Root
+{
+    public List<Person> people { get; set; }
+    public override string ToString()
+    {
+        string peopleStr = string.Join(Environment.NewLine, people.Select(p => p.ToString()));
+        return $"People:{Environment.NewLine}{peopleStr}";
+    }
+}
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await foo();
+    }
+    static async Task foo()
+    {
+        using (HttpClient connection = new HttpClient())
+        {
+            var response = await connection.GetStringAsync("https://localhost:7067/RequestControllers/GetAllStudents\r\n");
+            foreach (var student in JsonSerializer.Deserialize<List<Person>>(response))
+            {
+                Console.WriteLine(student);
+            }
+
+        }
+
+    }
+}
